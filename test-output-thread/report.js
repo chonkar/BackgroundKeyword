4323db010d3b72ca5cc9e7e698a268a5de865817
Kavita Chonkar <kavita.chonkar@aaseya.com>
2021-12-30 14:26:07 +0530

    running background keyword with testng

---
+++ test-output-thread/report.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "03dc6908-28e5-4656-b5db-bc7348751bd5",
+    "feature": "Guru99 login and create telecom project and add tariff plan",
+    "scenario": "telecom project and add tariff plan test scenario",
+    "start": 1640789762837,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1640789765655,
+    "className": "failed"
+  },
+  {
+    "id": "aadcc036-8a59-4a0e-85c7-34ec882230ef",
+    "feature": "Guru99 login and create telecom project and add tariff plan",
+    "scenario": "telecom project and add customers test scenario",
+    "start": 1640789746648,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1640789762805,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
